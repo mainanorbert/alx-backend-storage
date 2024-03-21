9795eb383dbdf4f3c85025e11d10ee6b605e6dc0
Norberts <mainanorbert@gmail.comm>
2024-03-21 21:20:12 +0300

    SQL script that creates a stored procedure ComputeAverageScoreForUser

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- script that creates a stored procedure ComputeAverageScoreForUser
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE average_score DECIMAL(10, 2);
+
+	SELECT AVG(score) INTO average_score
+	FROM corrections
+	WHERE user_id = user_id;
+	UPDATE users
+	SET average_score = average_score
+	WHERE id = user_id;
+END;//
+DELIMITER ;
