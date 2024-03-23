33b35954d7aafeb3d6d0ea935c30fb4cf5b1c0b0
Norberts <mainanorbert@gmail.comm>
2024-03-23 19:35:22 +0300

    script that creates a stored procedure

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -8,6 +8,8 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
 	DECLARE user_id INT;
+	DECLARE t_weight DECIMAL(10, 2);
+	DECLARE w_sum DECIMAL(10, 2);
 	DECLARE done INT DEFAULT FALSE;
 	DECLARE cur CURSOR FOR SELECT id FROM users;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -18,7 +20,22 @@ BEGIN
 		IF done THEN
 			LEAVE read_loop;
 		END IF;
-		CALL ComputeAverageWeightedScoreForUser(user_id);
+		-- CALL ComputeAverageWeightedScoreForUser(user_id);
+		-- DECLARE t_weight DECIMAL(10, 2);
+		-- DECLARE w_sum DECIMAL(10, 2);
+		SELECT SUM(weight * score), SUM(weight) INTO w_sum, t_weight
+		FROM corrections
+		JOIN projects ON corrections.project_id = projects.id
+		WHERE corrections.user_id = user_id;
+		IF t_weight > 0 THEN
+			UPDATE users
+			SET average_score = w_sum / t_weight
+			WHERE id = user_id;
+		ELSE
+			UPDATE users
+			SET average_score = 0
+			WHERE id = user_id;
+		END IF;
 	END LOOP;
 	CLOSE cur;
 END; //
