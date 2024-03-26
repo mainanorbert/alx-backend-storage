3c8da63915fc58204c46bdc301a573f949f75650
Norberts <mainanorbert@gmail.comm>
2024-03-26 13:08:26 +0300

    list of schools with python

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
-    result = mongo_collection.find({"topic": topic})
+    result = mongo_collection.find({"topics": topic})
     return list(result)
