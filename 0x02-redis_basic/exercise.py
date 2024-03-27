1a36020519d81f51dfc5f78689dabd75b8a455b8
Norberts <mainanorbert@gmail.comm>
2024-03-27 22:08:37 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ client as a private variable named _redis"""
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -15,8 +15,31 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data:Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """function takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float]:
+        """Reading from Redis and recovering original type"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """function that parametrize Cache.get
+        with the correct conversion function."""
+        par = self.get(key, fn=lambda d: d.decode('utf-8'))
+        return par
+
+    def get_int(self, key: str) -> Optional[int]:
+        """function that parametrize
+        Cache.get with the correct conversion function"""
+        par = self.get(key, fn=int)
+        return par
